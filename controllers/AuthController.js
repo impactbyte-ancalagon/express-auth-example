3b8ec5628da9c82f203af0ed05c4e5a48f50e79b
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-24 22:12:41 +0700

    Add HTTP status code for every response

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,41 +3,49 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 exports.signUp = async (req, res) => {
-  const salt = await bcrypt.genSalt(12)
+  try {
+    const salt = await bcrypt.genSalt(12)
 
-  req.body.password = await bcrypt.hash(req.body.password, salt)
+    req.body.password = await bcrypt.hash(req.body.password, salt)
 
-  await User.create(req.body)
+    await User.create(req.body)
 
-  res.json({ message: 'Success.' })
+    res.status(200).json({ message: 'Success.' })
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
+  }
 }
 
 exports.signIn = async (req, res) => {
-  const user = await User.findOne({ where: { email: req.body.email } })
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
 
-  if (user === null) {
-    return res.json({ message: 'User not found.' })
+    if (user === null) {
+      return res.status(403).json({ message: 'User not found.' })
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+    if (!validPassword) {
+      return res.status(403).json({ message: 'Password not valid.' })
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' }
+    )
+
+    res.status(200).json({
+      message: "You're logged in",
+      id: user.id,
+      token
+    })
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
   }
-
-  const validPassword = await bcrypt.compare(req.body.password, user.password)
-
-  if (!validPassword) {
-    return res.json({ message: 'Password not valid.' })
-  }
-
-  const token = jwt.sign(
-    { id: user.id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: '7d' }
-  )
-
-  res.json({
-    message: "You're logged in",
-    id: user.id,
-    token
-  })
 }
 
 exports.verifyToken = (req, res) => {
-  res.json({ user: req.decoded })
+  res.status(200).json({ user: req.decoded })
 }
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,33 +1,49 @@
 const { User } = require('../models')
 
 exports.getUsers = async (req, res) => {
-  const users = await User.findAll()
+  try {
+    const users = await User.findAll()
 
-  res.json({ users })
+    res.status(200).json({ users })
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
+  }
 }
 
 exports.getUserById = async (req, res) => {
-  const user = await User.findById(req.params.id)
+  try {
+    const user = await User.findById(req.params.id)
 
-  res.json({ user })
+    res.status(200).json({ user })
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
+  }
 }
 
 exports.updateUserById = async (req, res) => {
-  const [isUpdated] = await User.update(req.body, {
-    where: { id: req.params.id }
-  })
+  try {
+    const [isUpdated] = await User.update(req.body, {
+      where: { id: req.params.id }
+    })
 
-  if (Boolean(isUpdated)) {
-    const user = await User.findById(req.params.id)
+    if (Boolean(isUpdated)) {
+      const user = await User.findById(req.params.id)
 
-    res.json({ user })
-  } else {
-    res.json({})
+      res.status(200).json({ user })
+    } else {
+      res.status(400).json({})
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
   }
 }
 
 exports.deleteUserById = async (req, res) => {
-  await User.destroy({ where: { id: req.params.id } })
+  try {
+    await User.destroy({ where: { id: req.params.id } })
 
-  res.json({})
+    res.status(200).json({})
+  } catch (err) {
+    res.status(500).json({ message: 'There is an error.', err })
+  }
 }
